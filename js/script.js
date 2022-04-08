bfbafd4d624ddbcb15080c9a97d5fd93a24479da
Emilio Gonzales <emiliog1184@gmail.com>
2022-04-08 09:28:33 -0700

    Add html forms

--- js/script.js
+++ js/script.js
@@ -7,13 +7,16 @@ function Pizza(toppings, size) {
 }
 
 // prototype for pizza price
+// -- needs to change size price based on user input
 Pizza.prototype.pizzaPrice = function () {
-  let med = 10
-  return this.toppings.length * 3 + med;
+  const smal = 7;
+  const med = 10;
+  const lar = 13;
+  return this.toppings.length * 3 + lar;
 }
 
 // instance of pizza object
-const myPizza = new Pizza(['basil', 'mozzarella'], 'medio');
+const myPizza = new Pizza(['basil', 'mozzarella'], 'media');
 
 console.log(myPizza);
 
